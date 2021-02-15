aad881fcc69f62e9826b7e5741ca62eac40d35ca
Yehor Mushet <egor.mushet@gmail.com>
2021-02-16 00:37:14 +0200

    feat: 9 tests passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+function check(str, bracketsConfig) {
+    let stack = [];
+    let bracketsMap = {};
+    bracketsConfig.forEach(element => {
+        bracketsMap[element[0]] = element[1];
+    });
+    for (let i = 0; i < str.length; i++) {
+        const s = str[i];
+        console.log(s);
+        if (bracketsMap[s]) {
+            stack.push(s);
+        } else {
+            const popedBracket = stack.pop();
+            if (stack[popedBracket] != s) {
+                return false;
+            }
+        }
+        return stack.length === 0;
+    }
 }
+
+const config1 = [['(', ')']];
+console.log(check('()', config1));
