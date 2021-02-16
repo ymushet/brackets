1fc404f91a03737ceb75ee92cb642d9dd9fa5ee7
Yehor Mushet <egor.mushet@gmail.com>
2021-02-16 23:19:44 +0200

    feat: no equal open/close brackets solution (17/20)

--- src/index.js
+++ src/index.js
@@ -1,19 +1,38 @@
-module.exports =function check(str, bracketsConfig) {
+function checkCountMap(countMap) {
+    for (const key of countMap.keys()) {
+        if(countMap.get(key) % 2 != 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+module.exports = function check(str, bracketsConfig) {
     let stack = [];
-    let bracketsMap = {};
+    let bracketsMap = new Map();
     bracketsConfig.forEach(element => {
-        bracketsMap[element[0]] = element[1];
+        bracketsMap.set(element[0], element[1]);
     });
+    let countMap = new Map();
     for (let i = 0; i < str.length; i++) {
         const s = str[i];
-        if (bracketsMap[s]) {
-            stack.push(s);
+        if (bracketsMap.get(s) != undefined) {
+            if(bracketsMap.get(s) != s) {
+                stack.push(s);
+            }
+            else {
+                if(countMap.has(s)) {
+                    countMap.set(s, countMap.get(s) + 1);
+                } else {
+                    countMap.set(s, 1);
+                }
+            }
         } else {
             const popedBracket = stack.pop();
-            if (stack[popedBracket] != s) {
+            if (bracketsMap.get(popedBracket) != s) {
                 return false;
             }
         }
     }
-    return stack.length === 0;
+    return (stack.length === 0 && checkCountMap(countMap));
 }
