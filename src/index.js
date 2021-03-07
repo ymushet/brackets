59cae447f59dc99dff87183104d53b00e65d4ea7
Yehor Mushet <egor.mushet@gmail.com>
2021-03-07 14:58:55 +0200

    feat: new solution after help in discord

--- src/index.js
+++ src/index.js
@@ -1,38 +1,20 @@
-function checkCountMap(countMap) {
-    for (const key of countMap.keys()) {
-        if(countMap.get(key) % 2 != 0) {
-            return false;
-        }
-    }
-    return true;
-}
-
 module.exports = function check(str, bracketsConfig) {
-    let stack = [];
-    let bracketsMap = new Map();
-    bracketsConfig.forEach(element => {
-        bracketsMap.set(element[0], element[1]);
-    });
-    let countMap = new Map();
-    for (let i = 0; i < str.length; i++) {
-        const s = str[i];
-        if (bracketsMap.get(s) != undefined) {
-            if(bracketsMap.get(s) != s) {
-                stack.push(s);
-            }
-            else {
-                if(countMap.has(s)) {
-                    countMap.set(s, countMap.get(s) + 1);
-                } else {
-                    countMap.set(s, 1);
-                }
-            }
+    const open = bracketsConfig.map(x => x[0]);
+    const close = bracketsConfig.map(x => x[1]);
+    const stack = [];
+    for (let chr of str) {
+        let closeIndex = close.findIndex(el => el === chr);
+        let openIndex = open.findIndex(el => el === chr);
+        if (closeIndex === openIndex) {
+            if (stack.reduce((l, x) => l + (x === chr), 0) % 2 !== 0) openIndex = -1
+        }
+        if (openIndex !== -1) {
+            stack.push(chr);
         } else {
-            const popedBracket = stack.pop();
-            if (bracketsMap.get(popedBracket) != s) {
-                return false;
-            }
+            let lastOpener = stack.pop();
+            let trueOpener = open[closeIndex];
+            if (lastOpener !== trueOpener) return false;
         }
     }
-    return (stack.length === 0 && checkCountMap(countMap));
+    return stack.length === 0;
 }
